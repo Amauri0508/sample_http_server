b9be9739a6c69a87884b4fcfb8e5972ee2dd6367
chengang58 <983466943@qq.com>
2018-09-16 06:01:25 +0000

    eventloop

--- src/Worker/Worker.php
+++ src/Worker/Worker.php
@@ -1,6 +1,8 @@
 <?php
 namespace shs\Worker;
 
+use shs\EventLoop\LibEventLoop;
+use shs\EventLoop\EventLoopInterface;
 class Worker {
     public $stream;
     public $ip;
@@ -18,10 +20,13 @@ class Worker {
     public function __construct($ip, $port) {
         $this->ip = $ip;
         $this->port = $port;
+        $this->loop = new LibEventLoop();
     }
 
     public function listen() {
-
+        $this->loop->add($this->stream, EventLoopInterface::EV_READ, array($this, 'handleConnection'));
+        $this->loop->add(5, EventLoopInterface::EV_TIMER, array($this, 'clearTimedOutConnections'));
+        $this->loop->run();
     }
 
 
